e140f9ca90c8ce9a9e87f184303c205dc108bdb6
hoarf <alan.ficagna@gmail.com>
2013-11-14 09:19:35 -0200

    template novo

--- api.js
+++ api.js
@@ -1,26 +1,12 @@
-
 $(document).ready(function() {
   load_page();
 });
 
 function load_page () {
 
-  map = new OpenLayers.Map("demoMap");
+  map = new OpenLayers.Map("tour");
   map.addLayer(new OpenLayers.Layer.OSM());
   map.zoomToMaxExtent();
 
-  $("#select_query").select2( {width:"resolve"});
-  // var data = {
-  //     resource_id: '3076913f-38dd-4634-b26e-305c80d88576', // the resource id
-  //     limit: 5,
-  //   };
-  //   $.ajax({
-  //     url: 'http://datapoa.com.br/api/action/datastore_search',
-  //     data: data,
-  //     dataType: 'jsonp',
-  //     success: function(data) {
-  //       console.debug(data);
-  //     }
-  // }); 
- 
+  $("#select_query").select2({width:"element"});
 }
