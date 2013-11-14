b094f8874f4ed546fd5e2be078da62cb65330099
Lucas Zawacki <lfzawacki@gmail.com>
2013-11-14 00:52:00 -0200

    Adicionando as opções

--- api.js
+++ api.js
@@ -3,13 +3,49 @@ $(document).ready(function() {
   load_page();
 });
 
+var residue_options = {
+  "Pilhas e Baterias" : "",
+  "Eletrônicos" : "",
+  "Óleo de Cozinha" : "",
+  "Remédios" : "",
+  "Lâmpadas" : "",
+  "Pneus" : "",
+  "Madeiras" : "",
+  "Latas com resto de Tinta" : "",
+  "Móveis" : "",
+  "Colchôes" : "",
+  "Terra" : "",
+  "Madeiras" : "",
+  "Entulhos" : "",
+  "Caliça" : "",
+  "Cerâmica" : "",
+  "Sucatas de ferro" : "",
+  "Eletrodomésticos" : "",
+  "Resíduos arbóreos" : ""
+}
+
+function select_options() {
+  var opt = [];
+  var i = 0;
+
+  for (o in residue_options) {
+    opt.push( {id: i, text: o} )
+    i += 1;
+  }
+
+  return opt;
+}
+
 function load_page () {
 
-  map = new OpenLayers.Map("demoMap");
+  var map = new OpenLayers.Map("demoMap");
   map.addLayer(new OpenLayers.Layer.OSM());
   map.zoomToMaxExtent();
 
-  $("#select_query").select2( {width:"resolve"});
+  $("#select_query").select2({
+    width:"resolve",
+    data: select_options()
+  });
   // var data = {
   //     resource_id: '3076913f-38dd-4634-b26e-305c80d88576', // the resource id
   //     limit: 5,
@@ -23,4 +59,8 @@ function load_page () {
   //     }
   // }); 
  
+}
+
+function bind_menu() {
+
 }
