3f22e0f3aca98ac5852c068ab42f8db525279b5e
Mario Jr <mariogasparoni@gmail.com>
2013-12-19 12:16:30 -0200

    merge com master

--- js/api.js
+++ js/api.js
@@ -1,9 +1,17 @@
 function load_page () {
 
+  function format(state) {
+    return '<i class="btn btn-default btn-sm go-away-right fa fa-' + state.glyph + '"></i>' + state.text
+  };
+
   $("#select_query").select2({
     width: "element",
-    data: select_options()
+    data: select_options(),
+    formatResult: format,
+    formatSelection: format,
+    escapeMarkup: function(m) { return m; }
   });
+
 }
 
 $(document).ready(function() {
@@ -12,13 +20,17 @@ $(document).ready(function() {
 
   $("#select_query").on("change", function(e){
     residue_options[e.added.text]();
+    //$('.thumbnail img').attr('src', )
+    $('.thumbnail .caption h3').text(e.added.text);
+    $('.thumbnail .caption p').text(residue_text[e.added.text]);
+    $('.thumbnail #icone').attr('class', 'fa fa-' + residue_glyphs[e.added.text]+' fa-stack-1x fa-inverse');
   });
 
 });
 
 function tires()
 {
-  obtemLatitudesERenderizaNoMapa('data/pneus.json');
+  obtemLatitudesERenderizaNoMapa('data/pneus.json');  
 }
 
 function oil()
@@ -57,7 +69,6 @@ residue_options = {
   "Remédios" : medicine,
   "Lâmpadas" : where_bought,
   "Pneus" : tires,
-  "Madeiras" : ecopoint,
   "Latas com resto de Tinta" : ecopoint,
   "Móveis" : ecopoint,
   "Colchôes" : ecopoint,
@@ -67,8 +78,45 @@ residue_options = {
   "Caliça" : ecopoint,
   "Cerâmica" : ecopoint,
   "Sucatas de ferro" : ecopoint,
-  "Eletrodomésticos" : ecopoint,
   "Resíduos arbóreos" : ecopoint
+}
+
+residue_glyphs = {
+  "Pilhas e Baterias" : "bolt",
+  "Eletrônicos" : "laptop",
+  "Óleo de Cozinha" : "tint",
+  "Remédios" : "flask",
+  "Lâmpadas" : "search",
+  "Pneus" : "truck",
+  "Madeiras" : "search",
+  "Latas com resto de Tinta" : "search",
+  "Móveis" : "search",
+  "Colchôes" : "search",
+  "Terra" : "search",
+  "Entulhos" : "search",
+  "Caliça" : "search",
+  "Cerâmica" : "search",
+  "Sucatas de ferro" : "wrench",
+  "Resíduos arbóreos" : "leaf"
+}
+
+residue_text = {
+  "Pilhas e Baterias" : "Para realizar o descarte de pilhas devemos, primeiramente, tomar cuidado ao manuseá-las: Caso haja vazamentos ou indícios de ferrugem, é necessário utilizar uma luva de proteção, um jornal, ou...",
+  "Eletrônicos" : "Para descartar eletrônicos devemos",
+  "Óleo de Cozinha" : "Para descartarmos óleos de cozinha ...",
+  "Remédios" : "Para descartarmos Remédios",
+  "Lâmpadas" : "Para descartarmos Lampadas...",
+  "Pneus" : "truck",
+  "Madeiras" : "search",
+  "Latas com resto de Tinta" : "search",
+  "Móveis" : "search",
+  "Colchôes" : "search",
+  "Terra" : "search",
+  "Entulhos" : "search",
+  "Caliça" : "search",
+  "Cerâmica" : "search",
+  "Sucatas de ferro" : "wrench",
+  "Resíduos arbóreos" : "leaf"
 }
 
 function select_options() {
@@ -76,7 +124,7 @@ function select_options() {
   var i = 0;
 
   for (o in residue_options) {
-    opt.push( {id: i, text: o} )
+    opt.push( { id: i, text: o, glyph: residue_glyphs[o] } )
     i += 1;
   }
 
